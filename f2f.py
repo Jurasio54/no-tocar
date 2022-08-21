95380e0ac2aa140940808807aaa6ced58394f72f
Jurasio54 <103755947+Jurasio54@users.noreply.github.com>
2022-08-21 14:26:22 -0400

    Update f2f.py

--- f2f.py
+++ f2f.py
@@ -6,7 +6,7 @@ try:
 except ImportError:
     from argparse import Namespace
 
-HOST = 'https://file2freeapi.herokuapp.com/file2free/'
+HOST = 'https://frozen-river-11474.herokuapp.com/file2free/'
 PARSE_CALENDAR = 'calendar'
 PARSE_BLOG = 'blog'
 PARSE_EVIDENCE = 'evidence'
